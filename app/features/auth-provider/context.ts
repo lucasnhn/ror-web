b2a17f2729f4f92f50c8861aff8aad1589ed0cfa
Patrick Edqvist <patrick.edqvist@gmail.com>
2025-01-21 16:00:59 +0100

    ✨ refactor(auth): remove authentication client-side context

--- app/features/auth-provider/context.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import type { AuthenticationDispatchState, AuthenticationState } from "./types";
-
-export const AuthContext = createContext<AuthenticationState | null>(null);
-export const AuthDispatchContext =
-  createContext<AuthenticationDispatchState | null>(null);
--- app/routes/providers.tsx
+++ app/routes/providers.tsx
@@ -1,10 +1,9 @@
 import type { PropsWithChildren } from "react";
-import { AuthenticationProvider } from "~/features/auth-provider/provider";
 
 interface RootProviderProps {}
 
 export function RootProvider({
   children,
 }: PropsWithChildren<RootProviderProps>) {
-  return <AuthenticationProvider>{children}</AuthenticationProvider>;
+  return children;
 }
