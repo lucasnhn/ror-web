fc8ac807dd8ff9162a8f5739bf2e0a7f6c9eb347
Patrick Edqvist <patrick.edqvist@gmail.com>
2025-01-24 10:02:28 +0100

    feat: add missing app

---
+++ app/auth-guard.ts
@@ -0,0 +1,10 @@
+import { redirect } from "next/navigation";
+import { auth } from "./auth";
+
+export async function authGuard() {
+  const session = await auth();
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+  return session;
+}
---
+++ app/auth.ts
@@ -0,0 +1,38 @@
+import NextAuth, { DefaultSession } from "next-auth";
+import { Provider } from "next-auth/providers";
+import { env } from "../env";
+
+declare module "next-auth" {
+  interface Session {
+    accessToken: string;
+  }
+}
+
+const dexIdpProvider: Provider = {
+  type: "oidc",
+  id: "dex",
+  name: "dex",
+  issuer: env.AUTH_ISSUER,
+  clientId: env.AUTH_CLIENT_ID,
+  clientSecret: env.AUTH_CLIENT_SECRET,
+};
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [dexIdpProvider],
+  callbacks: {
+    jwt({ token, account }) {
+      if (account?.provider === "dex") {
+        return { ...token, accessToken: account.access_token };
+      }
+      return token;
+    },
+    session({ session, token }) {
+      session.accessToken = token.accessToken as string;
+      return session;
+    },
+    authorized: async ({ auth }) => {
+      // Logged in users are authenticated, otherwise redirect to login page
+      return !!auth;
+    },
+  },
+});
