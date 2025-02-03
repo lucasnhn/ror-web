3ab696f65e5416232e47f20541efd4010782b011
Patrick Edqvist <patrick.edqvist@gmail.com>
2025-02-03 08:51:57 +0100

    chore(app): test design and react library

--- apps/web/components/common/tile.tsx
+++ apps/web/components/common/tile.tsx
@@ -1,9 +1,25 @@
 import clsxm from "@/utils/clsxm";
-import type { ReactNode } from "react";
+import type { ReactElement, ReactNode } from "react";
 
 interface TileProps {
+  /**
+  * Layering tokens - 1, 2, 3
+  * @default 1
+  */
   layer: 1 | 2 | 3;
+  /**
+  * Render the component by your element of choice
+  * @example
+  * <Tile as="footer">
+  */
+  as?: ReactElement["type"];
+  /**
+  * Additional classes
+  */
   className?: string;
+  /**
+  * Content of the tile
+  */
   children: ReactNode;
 }
 
@@ -11,18 +27,20 @@ export function Tile({
   layer = 1,
   className,
   children,
+  as
 }: TileProps) {
   const classes = clsxm(
     {
-      [`bg-(--layer-01)`]: layer === 1,
-      [`bg-(--layer-02)`]: layer === 2,
-      [`bg-(--layer-03)`]: layer === 3,
+      [`bg-(--r-layer-01)`]: layer === 1,
+      [`bg-(--r-layer-02)`]: layer === 2,
+      [`bg-(--r-layer-03)`]: layer === 3,
     },
     className,
   );
+  const Element = as ?? "div"
   return (
-    <div className={classes}>
+    <Element className={classes}>
       {children}
-    </div>
+    </Element>
   );
 }
