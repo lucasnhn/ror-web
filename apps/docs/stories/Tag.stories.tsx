bff3a6e48bd85772c4f0892511bb167d2d206636
Hanna Lunne <hanna.lunne@nhn.no>
2025-03-18 09:40:35 +0100

    chore(docs): Resolve PR comments

--- apps/docs/stories/Tag.stories.tsx
+++ apps/docs/stories/Tag.stories.tsx
@@ -1,22 +1,23 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { Tag, TagProps } from '@ror/react/src/components/tag'
 import { ComponentType } from 'react'
+import { CheckIcon } from 'lucide-react'
 
 const meta = {
-  title: 'ui/Tag',
-  component: Tag as ComponentType<TagProps>,
-  parameters: {
-    layout: 'centered',
-  },
-  tags: ['autodocs'],
-  argTypes: {
-    className: { control: { type: 'text' }, description: 'Specify an optional className to be applied to the container node' },
-    size: { control: { type: 'select', options: ['sm', 'md', 'lg'] }, description: 'How large should the tag be?' },
-    variant: { control: { type: 'select', options: ['readonly', 'dismissible', 'operational', 'selectable'] }, description: 'What style of the tag should be used?' },
-    color: { control: { type: 'select', options: ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'gray'] }, description: 'What should the tag severity be? Affects the color of the tag.' },
-    icon: { control: false, description: 'What icon should be used?' },
-    children: { control: { type: 'text' }, description: 'What should the text for the tag be?' },
-  },
+    title: 'ui/Tag',
+    component: Tag as ComponentType<TagProps>,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    argTypes: {
+        className: { control: { type: 'text' }, description: 'Specify an optional className to be applied to the container node' },
+        size: { control: { type: 'select', options: ['sm', 'md', 'lg'] }, description: 'How large should the tag be?' },
+        variant: { control: { type: 'select', options: ['readonly', 'dismissible', 'operational', 'selectable'] }, description: 'What style of the tag should be used?' },
+        color: { control: { type: 'select', options: ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'gray'] }, description: 'What should the tag severity be? Affects the color of the tag.' },
+        icon: { control: false, description: 'What icon should be used?' },
+        children: { control: { type: 'text' }, description: 'What should the text for the tag be?' },
+    },
 } satisfies Meta<typeof Tag>
 
 export default meta
@@ -29,33 +30,30 @@ export const Default: Story = {
     args: {
         size: 'md',
         variant: 'readonly',
-        color: 'blue',
+        color: 'neutral',
         children: 'This is a default tag'
     },
 }
 
 /**
- * `sm` Tag story
+ * Story with `sm`, `md`, and `lg` sizes
  */
-export const SmallTag: Story = {
-    args: {
-        size: 'sm',
-        variant: 'readonly',
-        color: 'blue',
-        children: 'This is a small tag'
-    },
-}
-
-/**
- * `lg` Tag story
- */
-export const LargeTag: Story = {
-    args: {
-        size: 'md',
-        variant: 'readonly',
-        color: 'blue',
-        children: 'This is a large tag'
-    },
+export const SizesTags: Story = {
+    render: () => {
+        return (
+            <div
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: '0.5rem',
+                }}
+            >
+                <Tag size="sm">This is a small tag</Tag>
+                <Tag size="md">This is a medium tag</Tag>
+                <Tag size="lg">This is a large tag</Tag>
+            </div>
+        )
+    }
 }
 
 /**
@@ -63,116 +61,59 @@ export const LargeTag: Story = {
  */
 export const DismissibleTag: Story = {
     args: {
-        size: 'md',
         variant: 'dismissible',
-        color: 'blue',
         children: 'This is a dismissible tag'
     },
 }
 
 // TODO: Implement operational tag
-/**
- * `operational` Tag story
- */
-export const OperationalTag: Story = {
-    args: {
-        size: 'md',
-        variant: 'operational',
-        color: 'blue',
-        children: 'This is an operational tag'
-    },
-}
 
+/**
+ * `selectable` Tag story
+ */
 export const SelectableTag: Story = {
     args: {
         size: 'md',
         variant: 'selectable',
-        color: 'blue',
         children: 'This is a selectable tag'
     },
 }
 
 /**
- * `error` Tag story
- */
-export const ErrorTag: Story = {
-    args: {
-        size: 'md',
-        variant: 'readonly',
-        color: 'error',
-        children: 'This is an error tag'
-    },
-}
-
-/**
- * `success` Tag story
+ * Icon Tag story
  */
 export const SuccessTag: Story = {
     args: {
-        size: 'md',
-        variant: 'readonly',
-        color: 'success',
-        children: 'This is a success tag'
+        icon: <CheckIcon />,
+        children: 'This is a tag with an icon'
     },
 }
+
 
 /**
- * `warning` Tag story
+ * Story with tags with different colors
  */
-export const WarningTag: Story = {
-    args: {
-        size: 'md',
-        variant: 'readonly',
-        color: 'warning',
-        children: 'This is a warning tag'
-    },
+export const ColorTags: Story = {
+    render: () => {
+        return (
+            <div
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    flexWrap: 'wrap',
+                    gap: '0.5rem',
+                }}
+            >
+                <Tag color="red">This is a red tag</Tag>
+                <Tag color="orange">This is an orange tag</Tag>
+                <Tag color="yellow">This is a yellow tag</Tag>
+                <Tag color="green">This is a green tag</Tag>
+                <Tag color="blue">This is a blue tag</Tag>
+                <Tag color="purple">This is a purple tag</Tag>
+                <Tag color="pink">This is a pink tag</Tag>
+                <Tag color="gray">This is a gray tag</Tag>
+                <Tag color="neutral">This is a neutral tag</Tag>
+            </div>
+        )
+    }
 }
-
-/**
- * `info` Tag story
- */
-export const InfoTag: Story = {
-    args: {
-        size: 'md',
-        variant: 'readonly',
-        severity: 'blue',
-        children: 'This is an info tag'
-    },
-}
-
-/**
- * `caution-minor` Tag story
- */
-export const CautionMinorTag: Story = {
-    args: {
-        size: 'md',
-        variant: 'readonly',
-        color: 'caution-minor',
-        children: 'This is a caution-minor tag'
-    },
-}
-
-/**
- * `caution-major` Tag story
- */
-export const CautionMajorTag: Story = {
-    args: {
-        size: 'md',
-        variant: 'readonly',
-        color: 'caution-major',
-        children: 'This is a caution-major tag'
-    },
-}
-
-/**
- * `caution-undefined` Tag story
- */
-export const CautionUndefinedTag: Story = {
-    args: {
-        size: 'md',
-        variant: 'readonly',
-        color: 'caution-undefined',
-        children: 'This is a caution-undefined tag'
-    },
-}
-
