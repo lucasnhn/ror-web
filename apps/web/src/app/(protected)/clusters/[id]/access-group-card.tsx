2fec6bcd4108ffc5e0cd4fcc2c5ae5e3b8dc3e32
Patrick Edqvist <patrick.edqvist@gmail.com>
2025-04-02 14:17:23 +0200

    chore(web): remove console.log from access-group-card component

--- apps/web/src/app/(protected)/clusters/[id]/access-group-card.tsx
+++ apps/web/src/app/(protected)/clusters/[id]/access-group-card.tsx
@@ -1,28 +1,27 @@
 import clsxm from '@/utils/clsxm'
 import { Cluster } from '@ror/js-api-client'
-import { Stack } from '@ror/react'
-import { Layer } from '@ror/react/components/layer'
-import { Tile } from '@ror/react/components/tile'
+import { Stack, Layer, Tile } from '@ror/react'
+
 interface ClusterAccessGroupCardProps {
   cluster: Cluster
   className?: string
 }
 
 export function ClusterAccessGroupCard({ cluster, className }: ClusterAccessGroupCardProps) {
-
   const classes = clsxm('p-5', className)
-  console.log("cluster", cluster)
 
   return (
     <Tile className={classes}>
       <h3 className='heading-01 pb-3 mb-5 border-b border-b-(--r-border-subtle)'>Access Groups</h3>
       <Layer level={1}>
         <Stack gap={5} className='max-w-full'>
-            <ul className='list-disc list-inside '>
-                {cluster.acl.accessGroups.map((group) => (
-                    <li key={group} className='mb-3 last:mb-0' >{group}</li>
-                ))}
-            </ul>
+          <ul className='list-disc list-inside '>
+            {cluster.acl.accessGroups.map((group) => (
+              <li key={group} className='mb-3 last:mb-0'>
+                {group}
+              </li>
+            ))}
+          </ul>
         </Stack>
       </Layer>
     </Tile>
