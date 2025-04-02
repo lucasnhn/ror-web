19b73e1b30a9678863e36ec2eaf96b15bbab6eed
Patrick Edqvist <patrick.edqvist@gmail.com>
2025-04-02 23:14:00 +0200

    chore(js-api-client): remove lib directory

--- packages/js-api-client/lib/constants.ts
+++
@@ -1,4 +0,0 @@
-export const enum ResourceKind {
-  KubernetesCluster = 'KubernetesCluster',
-  Node = 'Node',
-}
--- packages/js-api-client/lib/index.ts
+++
@@ -1,9 +0,0 @@
-export { createApiClient } from './client-factory'
-export { loggingMiddleware } from './middlewares/logging'
-export { RorApiError, RorNotFoundError, RorForbiddenError, RorUnauthorizedError, isRorApiError } from './error'
-export type {
-  ClusterType as Cluster,
-  ClusterListItemType as ClusterListItem,
-} from './resources/clusters/clusters.types'
-export { Health } from './resources/generic-models/health'
-export type { Node, NodeResponse } from './resources/nodes/nodes.types'
