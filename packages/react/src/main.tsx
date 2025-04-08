d9370ff412341dc5ea5861324a1dd1475badbc43
Patrick Edqvist <patrick.edqvist@gmail.com>
2025-04-08 10:45:39 +0200

    chore: add severity icon to barrel files

--- packages/react/src/main.tsx
+++ packages/react/src/main.tsx
@@ -16,14 +16,8 @@ export { MetricsCard } from './components/metrics-card.tsx'
 export type { MetricsCardItem } from './components/metrics-card.tsx'
 export { MetricsWheel } from './components/metrics-wheel.tsx'
 export { Pagination } from './components/pagination.tsx'
+export { SeverityIcon } from './components/severity-icon.tsx'
 export { Stack } from './components/stack.tsx'
-export {
-  StructuredListRow,
-  StructuredListBody,
-  StructuredListCell,
-  StructuredListHead,
-  StructuredListWrapper,
-} from './components/structured-list.tsx'
 export {
   TableContainer,
   TableTitle,
