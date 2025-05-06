1f086de4205ba89f636c733e9565893b613468fe
Hanna Lunne <hanna.lunne@nhn.no>
2025-05-06 14:35:28 +0200

    refactor: remove unused and commented out code

--- packages/react/src/components/copy-button.tsx
+++ packages/react/src/components/copy-button.tsx
@@ -43,22 +43,8 @@ export interface CopyButtonProps {
   children?: ReactNode
 }
 
-// const DEFAULT_TOOLTIP_LABEL = 'Copy to clipboard'
-// const DEFAULT_TOOLTIP_FEEDBACK = 'Copied!'
-const DEFAULT_FEEDBACK_TIMEOUT = 2000
-
-export function CopyButton({
-  // tooltip = DEFAULT_TOOLTIP_LABEL,
-  // feedback = DEFAULT_TOOLTIP_FEEDBACK,
-  feedbackTimeout = DEFAULT_FEEDBACK_TIMEOUT,
-  onClick,
-  className,
-  children,
-  size = 'md',
-}: CopyButtonProps) {
+export function CopyButton({ onClick, className, children, size = 'md' }: CopyButtonProps) {
   const timeoutRef = useRef<NodeJS.Timeout | null>(null)
-  // const [tooltipOpen, setTooltipOpen] = useState(false)
-  // const [showFeedback, setShowFeedback] = useState(false)
 
   // Clean up timeout on unmount
   useEffect(() => {
@@ -70,42 +56,15 @@ export function CopyButton({
   }, [])
 
   const handleOnClick = () => {
-    // setShowFeedback(true)
-    // setTooltipOpen(true)
-
     if (typeof onClick === 'function') {
       onClick()
     }
-
-    timeoutRef.current = setTimeout(() => {
-      // setShowFeedback(false)
-    }, feedbackTimeout)
   }
-
-  const handleOnMouseEnter = () => {
-    // setTooltipOpen(true)
-  }
-
-  const handleOnMouseLeave = () => {
-    // setTooltipOpen(false)
-  }
-
-  // const tooltipLabel = showFeedback ? feedback : tooltip
   const classes = clsx('r-copy-btn', className)
 
   return (
-    // <Tooltip content={tooltipLabel} open={tooltipOpen} onOpenChange={setTooltipOpen}>
-    <Button
-      icon={<Copy />}
-      iconOnly
-      onMouseEnter={handleOnMouseEnter}
-      onMouseLeave={handleOnMouseLeave}
-      onClick={handleOnClick}
-      className={classes}
-      size={size}
-    >
+    <Button icon={<Copy />} iconOnly onClick={handleOnClick} className={classes} size={size}>
       {children}
     </Button>
-    // </Tooltip>
   )
 }
