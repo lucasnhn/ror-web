f823f879469c12516275d6a9c6a61787f328e23d
Hanna Lunne <hanna.lunne@nhn.no>
2025-05-06 14:40:04 +0200

    refactor: remove commented out code

--- apps/web/src/components/layout/app-shell/logo.tsx
+++ apps/web/src/components/layout/app-shell/logo.tsx
@@ -5,13 +5,11 @@ import { RorLogo } from '@/components/ui/ror-logo'
 
 export function Logo() {
   return (
-    // <Tooltip content='Homepage' side='right'>
     <Link
       href='/'
       className='w-8 h-8 cursor-pointer hover:bg-neutral-100 flex items-center justify-center rounded-sm shrink-0'
     >
       <RorLogo className='w-8 h-8' />
     </Link>
-    // </Tooltip>
   )
 }
