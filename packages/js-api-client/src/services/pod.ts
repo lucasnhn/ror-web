d810e717e20fa47ab27a7dc78f2511308b02328d
abjerke <anne.lise.bjerkestrand@nhn.no>
2025-05-28 07:22:40 +0200

    deleted createPodService

--- packages/js-api-client/src/services/pod.ts
+++
@@ -1,7 +0,0 @@
-import type { RequestOptions } from '../core/request'
-
-export const createPodsService = (Request: (RequestOptions: RequestOptions) => Promise<unknown>) => ({
-  list: async () => {},
-  byId: async () => {},
-  listByCluster: async () => {},
-})
