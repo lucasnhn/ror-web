3a6e12b81ef74beee64e151d5f12b7b36f6515fc
abjerke <anne.lise.bjerkestrand@nhn.no>
2025-05-28 11:00:30 +0200

    fix[js-api]: changed name in deployment schemas

--- packages/js-api-client/src/schemas/deployment.ts
+++ packages/js-api-client/src/schemas/deployment.ts
@@ -1,20 +1,16 @@
 import { z } from 'zod'
 import { createV2ResourceResponseSchema, V2ResourceSchema } from './common'
 
-const DeploymentStatus = z.object({
+const DeploymentStatusSchema = z.object({
   replicas: z.number(),
   availableReplicas: z.number(),
   readyReplicas: z.number(),
   updateReplicas: z.number(),
 })
 
-const DeploymnentSchema = z.object({
-  status: DeploymentStatus,
-})
-
 export const DeploymentSchema = V2ResourceSchema.extend({
   node: z.object({
-    status: DeploymnentSchema,
+    status: DeploymentStatusSchema,
   }),
 })
 
