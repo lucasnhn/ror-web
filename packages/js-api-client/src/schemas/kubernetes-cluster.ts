13dde68941ee4ff4d597ecf541bc20a7d7b5cd42
Hanna Lunne <hanna.lunne@nhn.no>
2025-06-16 14:43:03 +0200

    refactor(js-api-client): update schema to fit backend

--- packages/js-api-client/src/schemas/kubernetes-cluster.ts
+++ packages/js-api-client/src/schemas/kubernetes-cluster.ts
@@ -1,132 +1,172 @@
 import { z } from 'zod'
 import { createV2ResourceResponseSchema, V2ResourceSchema } from './common'
 
-export const ResourceMetadataOwnerReference = z.object({
-  apiVersion: z.string().nullable().optional(),
-  kind: z.string().nullable().optional(),
-  name: z.string().nullable().optional(),
-  uid: z.string().nullable().optional(),
+export const KubernetesClusterSpecData = z.object({
+  clusterId: z.string(),
+  provider: z.string(),
+  datacenter: z.string(),
+  region: z.string(),
+  zone: z.string(),
+  project: z.string(),
+  workspace: z.string(),
+  workorder: z.string(),
+  environment: z.string(),
 })
 
-export const Metadata = z.object({
-  name: z.string().nullable().optional(),
-  resourceVersion: z.string().nullable().optional(),
-  creationTimestamp: z.string().nullable().optional(),
-  labels: z.record(z.string(), z.string()).nullable().optional(),
-  annotations: z.record(z.string(), z.string()).nullable().optional(),
-  uid: z.string().nullable().optional(),
-  namespace: z.string().nullable().optional(),
-  generation: z.number().nullable().optional(),
-  ownerReferences: z.array(ResourceMetadataOwnerReference),
+// Extra metadata for machines
+export const KubernetesClusterSpecMetadataDetails = z.object({
+  labels: z.record(z.string(), z.string()), // stricter what you can have in label, so often used in selectors that give me those with this label
+  annotations: z.record(z.string(), z.string()), // less strict, often just additional information
 })
 
-export const SpecToolingConfig = z.object({
-  splunkIndex: z.string().nullable().optional(),
+export const KubernetesClusterStorage = z.object({
+  class: z.string(),
+  path: z.string(),
+  size: z.string(),
 })
 
-export const SpecProviderSpecTanzuSpec = z.object({
-  supervisorClusterName: z.string().nullable().optional(),
-  namespace: z.string().nullable().optional(),
+// Control plane: some machines in the cluster must hold processes internally in the cluster, so management. Often smaller because it only runs internal stuff. Master in master/slave terminology
+// TODO: CHECK CAUSE THIS IS CHANGED
+export const KubernetesClusterSpecControlPlane = z.object({
+  replicas: z.number(),
+  version: z.string(), // Kubernetes version
+  provider: z.string(),
+  machineClass: z.string(),
+  metadata: KubernetesClusterSpecMetadataDetails,
+  storage: z.array(KubernetesClusterStorage),
 })
 
-export const SpecProviderSpecAzureSpec = z.object({
-  subscriptionId: z.string().nullable().optional(),
-  resourceGroup: z.string().nullable().optional(),
+// If the user has enabled autoscaling, with configuration
+export const KubernetesClusterAutoscalingConfig = z.object({
+  enabled: z.boolean(),
+  minReplicas: z.number(),
+  maxReplicas: z.number(),
 })
 
-export const SpecProviderSpec = z.object({
-  tanzuSpec: SpecProviderSpecTanzuSpec,
-  azureSpec: SpecProviderSpecAzureSpec,
+// Extends autoscalingconfig, adds scaling rules
+const KubernetesClusterAutoscalingSpec = KubernetesClusterAutoscalingConfig.extend({
+  scalingRules: z.array(z.string()),
 })
 
-export const SpecTopologyControlPlane = z.object({
-  replicas: z.number().nullable().optional(),
-  version: z.string().nullable().optional(),
-  machineClass: z.string().nullable().optional(),
+// Store information about nodepool
+// TODO: CHECK CAUSE THIS IS CHANGED
+export const KubernetesClusterNodePool = z.object({
+  machineClass: z.string(),
+  provider: z.string(),
+  version: z.string(), // Kubernetes version
+  name: z.string(),
+  replicas: z.number(), // Number of nodes
+  autoscaling: KubernetesClusterAutoscalingSpec,
+  metadata: KubernetesClusterSpecMetadataDetails,
 })
 
-export const SpecTopologyWorkers = z.object({
-  name: z.string().nullable().optional(),
-  replicas: z.number().nullable().optional(),
-  version: z.string().nullable().optional(),
-  machineClass: z.string().nullable().optional(),
+// Worker is basically slave in master/slave terminology
+export const KubernetesClusterWorkers = z.object({
+  nodePools: z.array(KubernetesClusterNodePool),
 })
 
-export const SpecTopology = z.object({
-  controlPlane: SpecTopologyControlPlane.nullable().optional(),
-  workers: z.array(SpecTopologyWorkers).nullable().optional(),
+// Specification of the cluster, what is used to create it
+export const KubernetesClusterSpecTopology = z.object({
+  version: z.string(), // Kubernetes version
+  controlplane: KubernetesClusterSpecControlPlane, // Contains control plan configuration
+  workers: KubernetesClusterWorkers, // Contains worker nodes configuration
 })
 
-export const SpecEndpoint = z.object({
-  type: z.string().nullable().optional(),
-  address: z.string().nullable().optional(),
+export const KubernetesClusterSpec = z.object({
+  data: KubernetesClusterSpecData.nullable().optional(),
+  topology: KubernetesClusterSpecTopology.nullable().optional(),
 })
 
-export const Spec = z.object({
-  clusterId: z.string().nullable().optional(),
-  clusterName: z.string().nullable().optional(),
-  description: z.string().nullable().optional(),
-  project: z.string().nullable().optional(),
-  provider: z.string().nullable().optional(),
-  createdBy: z.string().nullable().optional(),
-  toolingConfig: SpecToolingConfig.nullable().optional(),
-  environment: z.string().nullable().optional(),
-  providerSpec: SpecProviderSpec.nullable().optional(),
-  topology: SpecTopology.nullable().optional(),
-  endpoints: z.array(SpecEndpoint).nullable().optional(), // TODO: check if this is removed in ror
+// cpu, memory, gpu, disk
+export const KubernetesClusterStatusClusterStatusResource = z.object({
+  capacity: z.string(),
+  used: z.string(),
+  percentage: z.number(),
 })
 
-export const StatusCondition = z.object({
-  type: z.string().nullable().optional(),
-  status: z.string().nullable().optional(),
-  lastTransitionTime: z.string().nullable().optional(),
-  reason: z.string().nullable().optional(),
-  message: z.string().nullable().optional(),
+export const KubernetesClusterStatusClusterStatusResources = z.object({
+  cpu: KubernetesClusterStatusClusterStatusResource.nullable().optional(),
+  memory: KubernetesClusterStatusClusterStatusResource.nullable().optional(),
+  gpu: KubernetesClusterStatusClusterStatusResource.nullable().optional(),
+  disk: KubernetesClusterStatusClusterStatusResource.nullable().optional(),
 })
 
-export const StatusClusterStatusPrice = z.object({
-  monthly: z.number().nullable().optional(),
-  yearly: z.number().nullable().optional(),
+export const KubernetesClusterNodePoolStatus = z.object({
+  name: z.string(),
+  status: z.string(),
+  message: z.string(),
+  scale: z.number(), // Number of nodes in the nodepool
+  machineClass: z.string(), // Size of machines in nodes
+  autoscaling: KubernetesClusterAutoscalingConfig,
+  resources: KubernetesClusterStatusClusterStatusResources,
 })
 
-export const StatusClusterStatusResource = z.object({
-  capacity: z.string().nullable().optional(),
-  used: z.string().nullable().optional(),
-  percentage: z.number().nullable().optional(),
+export const KubernetesClusterStatusPrice = z.object({
+  monthly: z.number(),
+  yearly: z.number(),
 })
 
-export const StatusClusterStatus = z.object({
-  price: StatusClusterStatusPrice,
-  nodePools: z.number().nullable().optional(),
-  nodes: z.number().nullable().optional(),
-  cpu: StatusClusterStatusResource,
-  memory: StatusClusterStatusResource,
-  gpu: StatusClusterStatusResource,
-  disk: StatusClusterStatusResource,
+export const KubernetesClusterControlPlaneStatus = z.object({
+  status: z.string(),
+  message: z.string(),
+  scale: z.number(),
+  machineClass: z.string(),
+  resources: KubernetesClusterStatusClusterStatusResources,
 })
 
-export const Status = z.object({
-  status: z.string().nullable().optional(),
-  endpoints: z.array(SpecEndpoint).nullable().optional(),
-  phase: z.string().nullable().optional(),
-  conditions: z.array(StatusCondition).nullable().optional(),
-  kubernetesVersion: z.string().nullable().optional(),
-  providerStatus: z.record(z.unknown()).nullable().optional(),
-  createdTime: z.string().nullable().optional(),
-  updatedTime: z.string().nullable().optional(),
-  lastObservedTime: z.string().nullable().optional(),
-  clusterStatus: StatusClusterStatus,
+export const KubernetesClusterClusterDetails = z.object({
+  externalId: z.string(), // Cluster's ID from the outside, for the ones who have made cluster, like Tanzu
+  resources: KubernetesClusterStatusClusterStatusResources,
+  price: KubernetesClusterStatusPrice,
+  controlplane: KubernetesClusterControlPlaneStatus,
+  nodepools: z.array(KubernetesClusterNodePoolStatus), // TODO: Check if this changed in ror repo
+})
+
+// NHN tooling, agent, kubernetes
+export const KubernetesClusterVersion = z.object({
+  name: z.string(),
+  version: z.string(),
+  branch: z.string(),
+})
+
+export const KubernetesClusterEndpoint = z.object({
+  name: z.string(), // Name of the endpoint, "controlplane", "kubernetes", "api", "dashboard, grafana, argocd", "datacenter"
+  address: z.string(), // Address of the endpoint
+})
+
+export const KubernetesClusterClusterState = z.object({
+  cluster: KubernetesClusterClusterDetails,
+  versions: z.array(KubernetesClusterVersion),
+  endpoints: z.array(KubernetesClusterEndpoint),
+  egressIP: z.string(), // IP that load balancers use to route traffic out of the cluster, used to reach the cluster from the outside, e.g. to firewalls
+  lastUpdated: z.coerce.date(),
+  lastUpdatedBy: z.string(),
+  created: z.coerce.date(),
+})
+
+export const KubernetesClusterCondition = z.object({
+  type: z.string(), // Type is the type of the condition. For example, "ready", "available", etc.
+  status: z.string(), // Status is the status of the condition. Valid vales are: ok, warning, error, working, unknown.
+  lastTransitionTime: z.string(), // LastTransitionTime is the last time the condition transitioned from one status to another.
+  reason: z.string(), // Reason is a brief reason for the condition's last transition.
+  message: z.string(), // Message is a human-readable message indicating details about the condition.
+})
+
+export const KubernetesClusterStatus = z.object({
+  state: KubernetesClusterClusterState,
+  phase: z.string(),
+  conditions: z.array(KubernetesClusterCondition),
 })
 
 export const KubernetesClusterSchema = V2ResourceSchema.extend({
-  // TODO: consider if the setup of z.object({kubernetesCluster: z.object({...})}) is the best way to structure this.
   kubernetescluster: z
     .object({
-      apiVersion: z.string(),
-      kind: z.string(),
-      metaData: Metadata,
-      spec: Spec,
-      status: Status.nullable().optional(),
+      // The spec defines how the cluster is configured and provisioned.
+      // e.g. how it should be
+      spec: KubernetesClusterSpec,
+      // The status defines the current state of the cluster.
+      // e.g. how it is
+      status: KubernetesClusterStatus.nullable().optional(),
     })
     .nullable()
     .optional(),
