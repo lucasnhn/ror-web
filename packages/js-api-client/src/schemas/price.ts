c2630e18b90f974743d2449799b2206a9043c18e
Hanna Lunne <hanna.lunne@nhn.no>
2025-08-26 11:00:02 +0200

    feat(packages): add api call for prices v1

---
+++ packages/js-api-client/src/schemas/price.ts
@@ -0,0 +1,18 @@
+import z from 'zod'
+import { createPaginationSchema } from './common'
+
+export const PriceSchema = z
+  .object({
+    cpu: z.number(),
+    from: z.string(),
+    id: z.string(),
+    machineClass: z.string(),
+    memory: z.number(),
+    memoryBytes: z.number(),
+    price: z.number(),
+    provider: z.string(),
+    to: z.string(),
+  })
+  .passthrough()
+
+export const PriceResponseSchema = createPaginationSchema(PriceSchema)
