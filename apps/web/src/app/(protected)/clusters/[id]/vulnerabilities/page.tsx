01bd4f57c926c20c9ea17776eb25c6ca6a44ff16
Hanna Lunne <hanna.lunne@nhn.no>
2025-10-10 14:46:45 +0200

    temp: commit before the weekend

--- apps/web/src/app/(protected)/clusters/[id]/vulnerabilities/page.tsx
+++ apps/web/src/app/(protected)/clusters/[id]/vulnerabilities/page.tsx
@@ -1,3 +1,12 @@
-export default function ClusterVulnerabilitiesPage() {
-  return <h1>Vulnerabilities</h1>
+import { CodeSnippet } from '@/components/ui/code-snippet'
+import { getRorApi } from '@/services/ror-api'
+import { VulnerabilityReport } from '@ror/js-api-client'
+
+export default async function ClusterVulnerabilitiesPage() {
+  const api = await getRorApi()
+  const listParams = new URLSearchParams()
+  const res = await api.datacenter.list(listParams)
+  const items: VulnerabilityReport[] = res?.resources ?? []
+
+  return <CodeSnippet type={'multi'}>{JSON.stringify(items, null, 2)}</CodeSnippet>
 }
