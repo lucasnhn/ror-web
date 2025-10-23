ba5e5e439993e339e9774cf52e17d4699d0f202d
Hanna Lunne <hanna.lunne@nhn.no>
2025-10-23 12:43:45 +0200

    refactor: remove selectedDisplayData from dependancy array

--- apps/web/src/hooks/use-display-data.ts
+++ apps/web/src/hooks/use-display-data.ts
@@ -36,7 +36,7 @@ export function useDisplayData<T>(domain: string) {
     } catch {
       // ignore
     }
-  }, [domain, selectedDisplayData])
+  }, [domain])
 
   // Keep localStorage updated only on the client
   useEffect(() => {
