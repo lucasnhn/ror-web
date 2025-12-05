6fbccc725b5a580590d26ee5943b9e121d20ca91
Hanna Lunne <hanna.lunne@nhn.no>
2025-12-05 14:26:36 +0100

    refactor: update cluster layouts

--- apps/web/src/features/cluster/config/cluster-details-layouts.ts
+++ apps/web/src/features/cluster/config/cluster-details-layouts.ts
@@ -1,10 +1,10 @@
-import { Layouts } from '@/utils/layout-item'
+import { ItemsLayouts } from '@/utils/layout-item'
 
-export const standardLayouts: Layouts = {
+export const standardLayouts: ItemsLayouts = {
   xl: [
     { i: 'memory', x: 6, y: 0, w: 2, h: 12, minW: 1, minH: 12 },
     { i: 'tools', x: 4, y: 0, w: 2, h: 12, minW: 1, minH: 12 },
-    { i: 'info', x: 0, y: 0, w: 4, h: 12, minW: 3, minH: 12 },
+    { i: 'info', x: 0, y: 0, w: 4, h: 12, minW: 2, minH: 12 },
     { i: 'versions', x: 8, y: 0, w: 2, h: 12, minW: 1, minH: 12 },
     { i: 'observed', x: 10, y: 6, w: 2, h: 6, minW: 1, minH: 6 },
     { i: 'prices', x: 10, y: 0, w: 2, h: 6, minW: 2, minH: 6 },
@@ -18,12 +18,12 @@ export const standardLayouts: Layouts = {
     { i: 'prices', x: 8, y: 10, w: 4, h: 9, minW: 4, minH: 4 },
   ],
   md: [
-    { i: 'memory', x: 3, y: 10, w: 3, h: 10, minW: 3, minH: 8 },
-    { i: 'tools', x: 8, y: 0, w: 4, h: 10, minW: 4, minH: 6 },
+    { i: 'memory', x: 4, y: 10, w: 4, h: 10, minW: 3, minH: 8 },
+    { i: 'tools', x: 8, y: 0, w: 4, h: 8, minW: 4, minH: 6 },
     { i: 'info', x: 0, y: 0, w: 8, h: 10, minW: 8, minH: 10 },
-    { i: 'versions', x: 0, y: 10, w: 3, h: 10, minW: 3, minH: 9 },
-    { i: 'observed', x: 6, y: 10, w: 3, h: 10, minW: 3, minH: 5 },
-    { i: 'prices', x: 9, y: 10, w: 3, h: 10, minW: 3, minH: 4 },
+    { i: 'versions', x: 0, y: 10, w: 4, h: 10, minW: 3, minH: 9 },
+    { i: 'observed', x: 8, y: 10, w: 4, h: 7, minW: 3, minH: 5 },
+    { i: 'prices', x: 8, y: 8, w: 4, h: 5, minW: 3, minH: 4 },
   ],
   sm: [
     { i: 'memory', x: 0, y: 10, w: 6, h: 10, minW: 6, minH: 10 },
