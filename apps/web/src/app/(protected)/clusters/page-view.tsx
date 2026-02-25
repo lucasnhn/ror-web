7079b2529845c322079efb0fe029c8d86d8c578a
Hanna Lunne <hanna.lunne@nhn.no>
2026-02-25 12:06:21 +0100

    refactor: remove test functionality in UI

--- apps/web/src/app/(protected)/clusters/page-view.tsx
+++ apps/web/src/app/(protected)/clusters/page-view.tsx
@@ -58,7 +58,6 @@ import { exportClustersAsCSV, exportClustersAsExcel } from '@/features/cluster/u
 import { Params } from '@/types/resources-page'
 import { useFilters } from '@/hooks/use-filters'
 import { SortDefinition, useSorting } from '@/hooks/use-sorting'
-import { TestApiKeys } from './testapikeys'
 
 /**
  * Props for the PageView component.
@@ -272,8 +271,6 @@ export const PageView = ({ className, user, clusters, params }: PageViewProps) =
         expect finished functionality or that all data is present. The development team is working hard on delivering a
         complete product as quick as possible :)
       </NotReadyMessage>
-
-      <TestApiKeys />
 
       <section className='px-12 my-8'>{params.view === 'list' ? <TableView /> : <GridView />}</section>
     </div>
