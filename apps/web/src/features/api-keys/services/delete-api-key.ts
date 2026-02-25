0bc4eadf178195e19d75ef58bf460ad3035739bf
Hanna Lunne <hanna.lunne@nhn.no>
2026-02-25 11:57:19 +0100

    temp: test delete api key

--- apps/web/src/features/api-keys/services/delete-api-key.ts
+++ apps/web/src/features/api-keys/services/delete-api-key.ts
@@ -4,6 +4,6 @@ import { getRorApi } from '@/services/ror-api'
 
 export async function deleteApiKey({ apikeyId }: { apikeyId: string }) {
   const api = await getRorApi()
-  const res = await api.apiKey.delete({ apikeyId })
+  const res = await api.apiKey.delete(apikeyId)
   return res
 }
